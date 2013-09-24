7937ee3f10b23d8b84c3940e3080982f751e9a6a
Brinda_Pathak <Brinda_Pathak@MYSHEC258721D.ad.infosys.com>
2013-09-24 10:22:58 +0530

    temp

---
+++ test/src/com/src/DrawView.java
@@ -0,0 +1,69 @@
+package com.src;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+//Brinda comment added
+
+public class DrawView extends View {
+	
+	Paint paint = new Paint();
+	int prevXID;
+	int prevYID;
+	Point point = new Point();
+
+	public DrawView(Context context, AttributeSet attrs) {
+
+		super(context, attrs);
+
+		setFocusable(true);
+		setFocusableInTouchMode(true);
+
+		paint.setStrokeWidth(5);
+		paint.setColor(Color.RED);
+		paint.setStrokeCap(Paint.Cap.ROUND);
+		paint.setStyle(Paint.Style.STROKE);
+
+		this.setOnTouchListener(new View.OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				v.setOnTouchListener(this);			
+				point.x = event.getX();
+				point.y = event.getY();
+				
+				invalidate();
+				return true;
+			}
+		});
+		requestFocus();
+	}
+
+	@Override
+	public void onDraw(Canvas canvas) {
+		//for (Point point : points) {
+			canvas.drawLine(20, 20, point.x, point.y, paint);			
+		//}
+	}
+
+	public void setPoints(Float xP, Float yP) {
+		Point point = new Point();
+		point.x = xP;
+		point.y = yP;
+		
+		postInvalidate();
+	}
+}
+
+class Point {
+	float x, y;
+
+	@Override
+	public String toString() {
+		return x + ", " + y;
+	}
+}
