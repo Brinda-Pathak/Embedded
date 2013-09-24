462e6b04ed654bf68f9e0a6a0370116235ce0f9c
Brinda_Pathak <Brinda_Pathak@MYSHEC258721D.ad.infosys.com>
2013-09-24 10:23:10 +0530

    first

---
+++ test/src/com/src/mainActivity.java
@@ -0,0 +1,31 @@
+package com.src;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class mainActivity extends Activity {
+
+	int x;
+	int y;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+
+	}
+
+	
+
+}
