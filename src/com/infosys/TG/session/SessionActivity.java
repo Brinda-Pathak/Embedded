8a455d562302973bed9fca0c2c2c48a072e1bf80
Brinda <Brinda_Pathak@MYSHEC258721D.ad.infosys.com>
2013-09-30 15:27:35 +0530

    test session

---
+++ src/com/infosys/TG/session/SessionActivity.java
@@ -0,0 +1,13 @@
+package com.infosys.TG.session;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SessionActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
