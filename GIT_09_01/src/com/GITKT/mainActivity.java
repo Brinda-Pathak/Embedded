5715bb9e3a115c9997ace6c43a1a1884e11b8951
Brinda_Pathak <Brinda_Pathak@MYSHEC258721D.ad.infosys.com>
2014-01-09 12:23:31 +0530

    commiting new branch

--- GIT_09_01/src/com/GITKT/mainActivity.java
+++ GIT_09_01/src/com/GITKT/mainActivity.java
@@ -4,12 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class mainActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        int i;
-        
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		int i, j = 10, k = 15;
+		i = j + k;
+
+	}
 }
