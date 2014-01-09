543878a920943752edb086957ef4f5fce61c024a
Brinda_Pathak <Brinda_Pathak@MYSHEC258721D.ad.infosys.com>
2014-01-09 12:21:15 +0530

    changed mainActivity

--- GIT_09_01/src/com/GITKT/mainActivity.java
+++ GIT_09_01/src/com/GITKT/mainActivity.java
@@ -9,5 +9,7 @@ public class mainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        int i;
+        
     }
 }
